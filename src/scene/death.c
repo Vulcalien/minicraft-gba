f9fab37994cf5f219978892d3d4120907bce6fbc
Vulcalien <vulcalien@vulcalien.net>
2024-02-05 16:37:17 +0100

    Add a respawn feature
    Previously, the player would lose all progress when dying and would be
    brought to the start menu. Now, the player can respawn in the same
    world, losing all the items in the inventory. This means that any change
    to the world (e.g. chests) will be retained after death.
    
    TODO: a setting that lets the player keep his inventory after death.

--- src/scene/death.c
+++ src/scene/death.c
@@ -1,4 +1,4 @@
-/* Copyright 2022 Vulcalien
+/* Copyright 2022, 2024 Vulcalien
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,6 +17,10 @@
 
 #include "input.h"
 #include "screen.h"
+#include "level.h"
+#include "entity.h"
+#include "tile.h"
+#include "sound.h"
 
 static u8 death_time;
 
@@ -24,18 +28,80 @@ static void death_init(u8 flags) {
     death_time = 0;
 }
 
+static inline void get_spawn_location(struct Level *level,
+                                      u32 *spawn_x, u32 *spawn_y) {
+    // try to find a grass tile
+    for(u32 i = 0; i < 65536; i++) {
+        u32 x = rand() % LEVEL_W;
+        u32 y = rand() % LEVEL_H;
+
+        u32 tile = LEVEL_GET_TILE(level, x, y);
+
+        if(tile == GRASS_TILE) {
+            *spawn_x = x;
+            *spawn_y = y;
+            return;
+        }
+    }
+
+    // Try to find any non-solid tile. This cannot fail, because at the
+    // very least stairs down are present.
+    for(u32 y = 0; y < LEVEL_H; y++) {
+        for(u32 x = 0; x < LEVEL_W; x++) {
+            const u32 tile_id = LEVEL_GET_TILE(level, x, y);
+            const struct Tile *tile = &tile_list[tile_id];
+
+            if(tile->is_solid)
+                continue;
+
+            // ignore stairs up: the player could remain stuck inside
+            // the surrounding box of hard rock
+            if(tile_id == STAIRS_UP_TILE)
+                continue;
+
+            *spawn_x = x;
+            *spawn_y = y;
+            return;
+        }
+    }
+}
+
+static inline void respawn(void) {
+    struct Level *level = &levels[3];
+
+    // remove all hostile entities from the level
+    for(u32 i = 1; i < ENTITY_LIMIT; i++) {
+        struct entity_Data *data = &level->entities[i];
+        if(data->type == ZOMBIE_ENTITY || data->type == SLIME_ENTITY)
+            data->type = -1;
+    }
+
+    // choose a new spawn position
+    u32 spawn_x = 0, spawn_y = 0;
+    get_spawn_location(level, &spawn_x, &spawn_y);
+
+    entity_add_player(level, spawn_x, spawn_y);
+
+    // TODO keep inventory
+
+    current_level = 3;
+}
+
 static void death_tick(void) {
     death_time++;
     if(death_time > 60) {
-        if(INPUT_CLICKED(KEY_A) || INPUT_CLICKED(KEY_B))
-            set_scene(&scene_start, 1);
+        if(INPUT_CLICKED(KEY_A) || INPUT_CLICKED(KEY_B)) {
+            SOUND_PLAY(sound_start);
+            respawn();
+            set_scene(&scene_game, 7);
+        }
     }
 }
 
 static void death_draw(void) {
-    const u8 death_x = 6;
+    const u8 death_x = 5;
     const u8 death_y = 5;
-    const u8 death_w = 18;
+    const u8 death_w = 20;
     const u8 death_h = 7;
 
     screen_draw_frame("", death_x, death_y, death_w, death_h);
@@ -47,7 +113,7 @@ static void death_draw(void) {
     screen_write("SCORE:", 4, death_x + 1, death_y + 3);
     SCREEN_WRITE_NUMBER(score, 10, 10, false, 8, death_x + 7, death_y + 3);
 
-    screen_write("PRESS A TO LOSE", 6, death_x + 1, death_y + 5);
+    screen_write("PRESS A TO RESPAWN", 6, death_x + 1, death_y + 5);
 }
 
 const struct Scene scene_death = {
