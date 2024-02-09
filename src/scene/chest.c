Vulcalien/minicraft-gba

commit 2b89ca46e5d3d2c38ce5708f0d5632b89785ad37

Quality: remove duplicated code for drawing chest interface
The code for drawing items inside the chest and the player's inventory
differed only for some variables.

src/scene/chest.c
@@ -1,4 +1,4 @@
-/* Copyright 2022 Vulcalien
+/* Copyright 2022, 2024 Vulcalien
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,20 +22,22 @@
 #include "player.h"
 #include "furniture.h"
 
-static i32 chest_selected[2] = { 0, 0 };
+static i32 selected[2] = { 0, 0 };
 static u8 chest_window;
 
 static struct Inventory *chest_inventory;
 
+THUMB
 static void chest_init(u8 flags) {
-    chest_selected[0] = 0;
-    chest_selected[1] = 0;
+    selected[0] = 0;
+    selected[1] = 0;
 
     chest_window = 0;
 
     chest_inventory = &chest_inventories[chest_opened_id];
 }
 
+THUMB
 static void chest_tick(void) {
     gametime++;
 
@@ -55,124 +57,95 @@ static void chest_tick(void) {
         return;
 
     if(INPUT_CLICKED(KEY_UP))
-        chest_selected[chest_window]--;
+        selected[chest_window]--;
     if(INPUT_CLICKED(KEY_DOWN))
-        chest_selected[chest_window]++;
+        selected[chest_window]++;
 
-    if(chest_selected[chest_window] < 0)
-        chest_selected[chest_window] = inv[0]->size - 1;
-    if(chest_selected[chest_window] >= inv[0]->size)
-        chest_selected[chest_window] = 0;
+    if(selected[chest_window] < 0)
+        selected[chest_window] = inv[0]->size - 1;
+    if(selected[chest_window] >= inv[0]->size)
+        selected[chest_window] = 0;
 
     if(INPUT_CLICKED(KEY_A)) {
         struct item_Data removed;
-        inventory_remove(inv[0], &removed, chest_selected[chest_window]);
+        inventory_remove(inv[0], &removed, selected[chest_window]);
 
         bool could_add;
         if(item_is_resource(removed.type)) {
             could_add = inventory_add_resource(
                 inv[1],
                 removed.type, removed.count,
-                chest_selected[chest_window ^ 1]
+                selected[chest_window ^ 1]
             );
         } else {
             could_add = inventory_add(
-                inv[1], &removed, chest_selected[chest_window ^ 1]
+                inv[1], &removed, selected[chest_window ^ 1]
             );
         }
 
         if(could_add) {
-            if(chest_selected[chest_window] == inv[0]->size &&
+            if(selected[chest_window] == inv[0]->size &&
                inv[0]->size > 0) {
-                chest_selected[chest_window]--;
+                selected[chest_window]--;
             }
         } else {
             // put the item back into the current inventory
-            inventory_add(inv[0], &removed, chest_selected[chest_window]);
+            inventory_add(inv[0], &removed, selected[chest_window]);
         }
     }
 }
 
+THUMB
 static void chest_draw(void) {
-    const u8 chest_x = 2 + (chest_window == 0) * 2;
-    const u8 chest_y = 2;
-    const u8 chest_w = 12;
-    const u8 chest_h = 14;
-
-    const u8 inv_x = 16 - (chest_window == 1) * 2;
-    const u8 inv_y = 2;
-    const u8 inv_w = chest_w;
-    const u8 inv_h = chest_h;
-
     // clear the screen (fully transparent)
     for(u32 y = 0; y < 18; y++)
         for(u32 x = 0; x < 30; x += 2)
             *((vu32 *) &BG3_TILEMAP[x + y * 32]) = 0;
 
-    screen_draw_frame("CHEST", chest_x, chest_y, chest_w, chest_h);
-    screen_draw_frame("INVENTORY", inv_x, inv_y, inv_w, inv_h);
+    for(u32 frame = 0; frame < 2; frame++) {
+        u8 frame_x;
+        const u8 frame_y = 2;
+        const u8 frame_w = 12;
+        const u8 frame_h = 14;
 
-    // draw chest items
-    {
-        i8 item0 = chest_selected[0] - (chest_h - 2) / 2;
-        if(item0 > chest_inventory->size - (chest_h - 2))
-            item0 = chest_inventory->size - (chest_h - 2);
+        struct Inventory *inventory;
+
+        if(frame == 0) {
+            frame_x = 2 + (chest_window == 0) * 2;
+            inventory = chest_inventory;
+        } else {
+            frame_x = 16 - (chest_window == 1) * 2;
+            inventory = &player_inventory;
+        }
+
+        // draw frame
+        screen_draw_frame(
+            (frame == 0 ? "CHEST" : "INVENTORY"),
+            frame_x, frame_y, frame_w, frame_h
+        );
+
+        // draw items
+        i8 item0 = selected[frame] - (frame_h - 2) / 2;
+        if(item0 > inventory->size - (frame_h - 2))
+            item0 = inventory->size - (frame_h - 2);
         if(item0 < 0)
             item0 = 0;
 
-        for(u32 i = 0; i < chest_h - 2; i++) {
-            if(item0 + i >= chest_inventory->size)
+        for(u32 i = 0; i < frame_h - 2; i++) {
+            if(item0 + i >= inventory->size)
                 break;
 
-            struct item_Data *data = &chest_inventory->items[item0 + i];
+            struct item_Data *data = &inventory->items[item0 + i];
 
-            item_draw_icon(data, chest_x + 1, chest_y + 1 + i, false);
-            item_write(data, 4, chest_x + 2, chest_y + 1 + i);
-        }
+            item_draw_icon(data, frame_x + 1, frame_y + 1 + i, false);
+            item_write(data, 4, frame_x + 2, frame_y + 1 + i);
 
-        if(chest_window == 0) {
-            screen_write(
-                ">", 4,
-                chest_x,
-                chest_y + 1 + (chest_selected[0] - item0)
-            );
-            screen_write(
-                "<", 4,
-                chest_x + chest_w - 1,
-                chest_y + 1 + (chest_selected[0] - item0)
-            );
-        }
-    }
-
-    // draw inventory items
-    {
-        i8 item0 = chest_selected[1] - (inv_h - 2) / 2;
-        if(item0 > player_inventory.size - (inv_h - 2))
-            item0 = player_inventory.size - (inv_h - 2);
-        if(item0 < 0)
-            item0 = 0;
-
-        for(u32 i = 0; i < inv_h - 2; i++) {
-            if(item0 + i >= player_inventory.size)
-                break;
-
-            struct item_Data *data = &player_inventory.items[item0 + i];
-
-            item_draw_icon(data, inv_x + 1, inv_y + 1 + i, false);
-            item_write(data, 4, inv_x + 2, inv_y + 1 + i);
-        }
-
-        if(chest_window == 1) {
-            screen_write(
-                ">", 4,
-                inv_x,
-                inv_y + 1 + (chest_selected[1] - item0)
-            );
-            screen_write(
-                "<", 4,
-                inv_x + inv_w - 1,
-                inv_y + 1 + (chest_selected[1] - item0)
-            );
+            // draw cursor arrows
+            if(chest_window == frame) {
+                u32 cursor_y = frame_y + 1 + (selected[frame] - item0);
+                screen_write(">", 4, frame_x, cursor_y);
+                screen_write("<", 4, frame_x + frame_w - 1, cursor_y);
+            }
         }
     }
 }

commit 4064d86436d231ad3da7012927a8562552882412

Fix: chest cursor is drawn as many times as there are items
This bug was introduced in commit 2b89ca4.

Beside worsening performance, the only visible change was that when an
inventory was empty (the chest's or the player's) the cursor for that
inventory would not be drawn at all.

src/scene/chest.c
@@ -139,13 +139,13 @@ static void chest_draw(void) {
 
             item_draw_icon(data, frame_x + 1, frame_y + 1 + i, false);
             item_write(data, 4, frame_x + 2, frame_y + 1 + i);
+        }
 
-            // draw cursor arrows
-            if(chest_window == frame) {
-                u32 cursor_y = frame_y + 1 + (selected[frame] - item0);
-                screen_write(">", 4, frame_x, cursor_y);
-                screen_write("<", 4, frame_x + frame_w - 1, cursor_y);
-            }
+        // draw cursor arrows
+        if(chest_window == frame) {
+            u32 cursor_y = frame_y + 1 + (selected[frame] - item0);
+            screen_write(">", 4, frame_x, cursor_y);
+            screen_write("<", 4, frame_x + frame_w - 1, cursor_y);
         }
     }
 }